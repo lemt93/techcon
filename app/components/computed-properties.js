33bd400b8314390573204d1f31178a48eea9385f
Tam Le <lemt93@gmail.com>
2016-08-22 19:54:42 +0700

    Add computed-properties example

--- app/components/computed-properties.js
+++ app/components/computed-properties.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   ember: false,
-  react: false,
+  react: true,
   angular: true,
 
   cool: Ember.computed.and('react', 'angular'),
-  awesome: Ember.computed.and('cool', 'ember')
+  awesome: Ember.computed.and('cool', 'ember'),
+  someValue: Ember.computed('awesome', function () {
+    return this.get('awesome') ? 'Awesome!' : 'Cool.';
+  })
 });
